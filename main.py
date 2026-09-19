98666ee039e103b780bb968648aee38831e897b2
yourashishx074-netizen <yourashishx074@gmail.com>
2026-09-19 06:01:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 # --- CONFIGURATION ---
 API_ID = int(os.getenv("API_ID", "36055068"))
 API_HASH = os.getenv("API_HASH", "e62c399663de4721efb786f7cfc64022")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8836214965:AAHxuAaPPWPZi-GcTcPFRMRUidPJA5z1Dsk")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8636389576:AAGDv3vzvBmzlpfCRnVY8NkdOkcXyg-MyeE")
 OWNER_ID = int(os.getenv("OWNER_ID", "8882297263"))
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://getochammarhuyarr18_db_user:JSuaAzEfIlkVJWMO@cluster0.oakct2r.mongodb.net/?appName=Cluster0")
 LOGGER_ID = int(os.getenv("LOGGER_ID", "-1004423792133"))
